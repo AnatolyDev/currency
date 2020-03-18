c6f506875772709919120136b84f2095db58757b
Anatoly <tolik_83@mail.ru>
2020-03-18 22:28:49 +0300

    Первая версия

---
+++ build/precache-manifest.30cca624d97d0d1e49abd4b7b7db9e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ca03d6f018a6635a4a0d42838ed8ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "279224b73983101644c5",
+    "url": "/static/css/main.554434bf.chunk.css"
+  },
+  {
+    "revision": "6a95ac6861f88bca556b",
+    "url": "/static/js/2.bde17b52.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.bde17b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279224b73983101644c5",
+    "url": "/static/js/main.46add960.chunk.js"
+  },
+  {
+    "revision": "d3983878745c24015c37",
+    "url": "/static/js/runtime-main.b671c2c1.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -3,38 +3,58 @@ import './App.css';
 import axios from 'axios';
 import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
 
+/*const arr = [
+  {x:1, y:10},
+  {x:2, y:15},
+  {x:3, y:12},
+  {x:4, y:17},
+  {x:5, y:15}
+]*/
+
 const App = () => {
 
-  const [i, setI] = useState(0);
-
+  // хуки для хранения текущих курсов валют 
   const [USDcur, setUSDcur] = useState(undefined);
   const [RURcur, setRURcur] = useState(undefined);
   const [EURcur, setEURcur] = useState(undefined);
 
+  // хуки для хранения массивов курсов валют
   const [USDarr, setUSDarr] = useState([]);
   const [RURarr, setRURarr] = useState([]);
   const [EURarr, setEURarr] = useState([]);
 
-  const writeUSD = (x) => {
-    setUSDcur(x);
-  }
+  const [errorMes, setErrorMes] = useState(undefined);
 
-  const getCurrency = () => {
+  // основная функция чтения курсов из АПИ и складывания их в хуки
+  const getCurrency = (startD) => {
+
     async function getDataFromAPI(url) {
       try {
         const data = await axios.get(url);
-        console.log(data.data.Valute);
+
         if (data.data && data.data.Valute && data.data.Valute.USD && data.data.Valute.USD.Value) {
-          writeUSD(data.data.Valute.USD.Value);
-          setUSDarr([...USDarr, {x : i, y : data.data.Valute.USD.Value}]);
+          setUSDcur(data.data.Valute.USD.Value);
+          setUSDarr(prevArr => [...prevArr, {x : (new Date()), y : data.data.Valute.USD.Value}]);
         } else {
           setUSDcur(undefined)
         }
         
-        data.data && data.data.Valute && data.data.Valute.RUR && data.data.Valute.RUR.Value ? setRURcur(data.data.Valute.RUR.Value) : setRURcur(undefined);
-        data.data && data.data.Valute && data.data.Valute.EUR && data.data.Valute.EUR.Value ? setEURcur(data.data.Valute.EUR.Value) : setEURcur(undefined);
+        if (data.data && data.data.Valute && data.data.Valute.RUR && data.data.Valute.RUR.Value) {
+          setRURcur(data.data.Valute.RUR.Value);
+          setRURarr(prevArr => [...prevArr, {x : (new Date()), y : data.data.Valute.RUR.Value}]);
+        } else {
+          setRURcur(undefined);
+        }
+
+        if (data.data && data.data.Valute && data.data.Valute.EUR && data.data.Valute.EUR.Value) {
+          setEURcur(data.data.Valute.EUR.Value);
+          setEURarr(prevArr => [...prevArr, {x : (new Date()), y : data.data.Valute.EUR.Value}]);
+        } else {
+          setEURcur(undefined);
+        }
       } catch(error) {
-        console.log(error)
+        console.log(error);
+        error.message ? setErrorMes(error.message) : setErrorMes('Ошибка запроса данных');
       };
     }
     getDataFromAPI('https://www.cbr-xml-daily.ru/daily_json.js');
@@ -42,11 +62,12 @@ const App = () => {
 
   useEffect(
     () => {
+      const startDate = new Date();
       const timerId = setInterval(
         () => {
-          getCurrency();
+          getCurrency(startDate);
         },
-        2000
+        5000
       );
       return () => {
         clearInterval(timerId);
@@ -62,13 +83,13 @@ const App = () => {
           <caption>Таблица курсов валют</caption>
           <thead>
             <tr>
-              <th>
+              <th style={{backgroundColor:'red'}}>
                 USD
               </th>
               <th>
                 RUR
               </th>
-              <th>
+              <th style={{backgroundColor:'green'}}>
                 EUR
               </th>
             </tr>
@@ -82,19 +103,50 @@ const App = () => {
                 {RURcur ? RURcur : '=='}
               </td>
               <td>
-                {EURcur}
+                {EURcur ? EURcur : '=='}
               </td>
             </tr>
           </tbody>
         </table>
+        
       </div>
-      <XYPlot height={300} width= {300}>
-        <VerticalGridLines />
-        <HorizontalGridLines />
-        <XAxis />
-        <YAxis />
-        <LineSeries data={USDarr} />
-      </XYPlot>
+      <hr />
+      {errorMes &&
+        <h2 className='error-mes'>{errorMes}</h2>
+      }
+      {!errorMes &&
+        <XYPlot height={300} width= {800} yDomain={[0, 100]} className='trend-area' >
+          <VerticalGridLines style={{stroke: '#B7E9ED'}} />
+          <HorizontalGridLines style={{stroke: '#B7E9ED'}} />
+          <XAxis 
+              tickFormat={function tickFormat(d){
+                const date = new Date(d)
+                return date.getHours() + ':' + date.getMinutes()
+                }}
+              title="Время"
+              style={{
+                line: {stroke: '#ADDDE1'},
+                ticks: {stroke: '#ADDDE1'},
+                text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+              }}
+              tickTotal={10}
+          />
+          <YAxis 
+            
+          />
+          <LineSeries
+            className="USD"
+            color = "red"
+            data={USDarr} 
+          />
+
+          <LineSeries
+            className="EUR"
+            color = "green"
+            data={EURarr} 
+          />
+        </XYPlot>
+      }
     </div>
   );
 }
